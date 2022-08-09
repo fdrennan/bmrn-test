51116745f19fd38be9c82a068965fb813a293993
Freddy <drennanfreddy@gmail.com>
2022-08-09 17:47:59 -0600

    save

--- R/analysis_a_report.R
+++ R/analysis_a_report.R
@@ -7,7 +7,7 @@ ui_analysis_a_report <- function(id = "analysis_a_report", session_id = "") {
     div(class = "col-xl-4 col-lg-4 col-md-4"),
     div(
       class = "col-xl-4 col-lg-4 col-md-4 d-flex justify-content-center",
-      uiOutput(ns('emailUI')),
+      # uiOutput(ns('emailUI')),
       actionButton(ns("runReport"), "Run Report", class = "btn btn-primary")
     )
   )
@@ -30,6 +30,7 @@ server_analysis_a_report <- function(id = "analysis_a_report", server_input) {
       })
       
       output$emailUI <- renderUI({
+        req(server_input)
         data <- req(data())
         email <- data$email
         selectizeInput(ns('repemail'), 'Email', selected = email, choices=email, options = list(create = TRUE), multiple=TRUE)
@@ -38,7 +39,7 @@ server_analysis_a_report <- function(id = "analysis_a_report", server_input) {
       observeEvent(input$runReport, {
         showNotification("Making Report")
         data <- req(data())
-        req(input$repemail)
+        # req(input$repemail)
         name <- data$name
         uuid <- data$uuid
         email <- data$email
@@ -83,8 +84,8 @@ server_analysis_a_report <- function(id = "analysis_a_report", server_input) {
 
         tryCatch(
           {
-            send_email(all_files = TRUE, to = input$email, files = files, email_message = email_message)
-            send_email(all_files = FALSE, to = input$email, files = files, email_message = email_message)
+            send_email(all_files = TRUE, to = email, files = files, email_message = email_message)
+            send_email(all_files = FALSE, to = email, files = files, email_message = email_message)
 
             if (!getOption("send")) {
               showNotification("Using development settings, not sending email")
