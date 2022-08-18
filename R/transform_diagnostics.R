6178f2102cdfa57c8efeeaa1ba1027297030e622
cwilso6 <cmwilson0109@gmail.com>
2022-08-18 14:44:17 -0400

    Check dunnett's v. mvt

--- R/transform_diagnostics.R
+++ R/transform_diagnostics.R
@@ -2,6 +2,7 @@
 #' @export transform_diagnostics
 
 transform_diagnostics <- function(analysis_data, baseline) {
+  
   transformed <- transformation_check(analysis_data)
   transformed_data <- transformed$transformed
   power <- transformed$bc_transformation
