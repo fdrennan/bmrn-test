c1f80a0a1cae3a58881e98cee53ff3d004dfa13d
Freddy <drennanfreddy@gmail.com>
2022-09-26 16:20:56 -0600

    save

--- functions/box/app.r
+++ functions/box/app.r
@@ -14,19 +14,31 @@ collapser <- function(target = NULL, ..., class = "btn btn-primary") {
 
 #' @export
 ui <- function() {
-  box::use(shiny, shiny[tags], shinyjs)
-  box::use(./app)
+  box::use(shiny, shinyjs, shinycssloaders)
   shiny$addResourcePath("loaders", "./www/images/loaders")
   shiny$fluidPage(
     shinyjs$useShinyjs(),
     shiny$includeCSS("./www/styles.css"),
     shiny$includeScript("node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"),
+    shiny$uiOutput('app', container = function(...) {
+      shiny$fluidRow(class = "vh-100", shiny$column(
+        12, ...
+      ))
+    })
+  )
+}
+
+#' @export
+server <- function(input, output, session) {
+  box::use(shiny, shinyjs, shiny[tags], )
+  box::use(./app)
+  output$app <- shiny$renderUI({
     shiny$fluidRow(
       class = "vh-100",
       shiny$div(
         id = "sidebar", class = "col-3",
         class = "bg-dark",
-        app$collapser(".collapseExample", "Collapse"),
+        app$collapser(target = ".collapseExample", "Collapse"),
         shiny$actionButton("closeSidebar", "Close Sidebar")
       ),
       shiny$div(
@@ -35,18 +47,16 @@ ui <- function() {
         tags$h1("Hello", id = "hello")
       )
     )
-  )
-}
-
-#' @export
-server <- function(input, output, session) {
-  box::use(shiny, shinyjs)
+  })
 }
 
 #' @export
 start <- function() {
   box::use(shiny)
   box::use(./app)
+  # browser()
+  debug(app$ui)
+  debug(app$server)
   shiny$runApp(
     shiny$shinyApp(app$ui, app$server)
   )
