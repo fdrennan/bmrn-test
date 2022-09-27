268bc02d059520c9bfb80010fa28e294a98803b6
Freddy <drennanfreddy@gmail.com>
2022-09-26 20:54:37 -0600

    save

--- functions/box/app.r
+++ functions/box/app.r
@@ -1,32 +1,70 @@
+#' @example
+#' collapser(id ='asdf'),
+#' shiny$div(
+#'  id = "asdf", class = "collapse show",
+#'  tags$div("Hello", class = "card card-body")
+#' )
+#' @export
+collapser <- function(id = NULL,
+                      data_bs_toggle = c("collapse", "offcanvas"),
+                      class = "btn btn-primary", ...) {
+  data_bs_toggle <- match.arg(data_bs_toggle)
+  box::use(shiny[tags, tag])
+  tag("button", varArgs = list(
+    class = class,
+    type = "button",
+    `data-bs-toggle` = data_bs_toggle,
+    `data-bs-target` = paste0("#", id),
+    `aria-expanded` = "false",
+    `aria-controls` = id,
+    tags$h4("Submit"),
+    ...
+  ))
+}
+
+#' @export
+offcanvas <- function() {
+  box::use(shiny, shiny[tags])
+  shiny$fluidRow(
+    shiny$column(12, collapser(id = "offcanvasScrolling", data_bs_toggle = "offcanvas")),
+    tags$div(
+      class = "offcanvas offcanvas-start",
+      `data-bs-scroll` = "true",
+      `data-bs-backdrop` = "false",
+      tabindex = "-1",
+      id = "offcanvasScrolling",
+      `aria-labelledby` = "offcanvasScrollingLabel",
+      tags$div(
+        class = "offcanvas-header",
+        tags$h5(class = "offcanvas-title", id = "offcanvasScrollingLabel", "Colored with scrolling"),
+        tags$button(class = "btn-close text-reset", `data-bs-dismiss` = "offcanvas", `aria-label` = "Close")
+      ),
+      tags$div(class = "offcanvas-body", tags$p("Try scrolling the rest of the page to see this option in action."))
+    )
+  )
+}
 
 #' @export
 ui <- function() {
   box::use(shiny, shinyjs, shinycssloaders)
-  box::use(shiny[tags])
+  box::use(shiny[tags, HTML])
   box::use(. / app)
   shiny$addResourcePath("loaders", "./www/images/loaders")
   shiny$fluidPage(
     shinyjs$useShinyjs(),
     shiny$includeCSS("./www/styles.css"),
     shiny$includeScript("node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"),
+    shiny$fluidRow(app$offcanvas()),
     shiny$fluidRow(
-      class = "vh-100",
-      shiny$div(id = "sidebar",
-        class = "col-3 p-3 bg-dark",
-        shiny::tag("button", varArgs = list(
-          class = "btn btn-primary btn-block",
-          type = "button",
-          `data-bs-toggle` = "collapse",
-          `data-bs-target` = "#body",
-          `aria-expanded` = "false",
-          `aria-controls` = "body",
-          tags$h4("Submit")
-        )),
-        tags$div(class='collapse', tags$p("Ok, so here is some text", class = "bg-light"))
-      ),
+      # class = "vh-100",
       shiny$div(
-        id = "body", class = "collapse show col-9",
-        tags$div("Hello", class = "card card-body")
+        id = "body", class = "col-9 p-3",
+        shiny$fluidRow(
+          lapply(
+            1:9,
+            function(x) tags$div(tags$h3("Body", class = "display-3"), class = "col-3 card card-body")
+          )
+        )
       )
     )
   )
