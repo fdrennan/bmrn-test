e27f31462c96f71ee17010704d0afac62f35a1d2
Freddy <drennanfreddy@gmail.com>
2022-10-04 00:33:17 +0000

    *  Author: freddy  *  Created on: Tue 04 Oct 2022 12:33:17 AM UTC

--- functions/app.r
+++ functions/app.r
@@ -11,6 +11,7 @@ app_ui <- function(id = "app") {
       shinyjs[useShinyjs, extendShinyjs],
       . / reddit,
       . / offcanvas,
+      . / header,
       . / button,
       esquisse,
       . / utilities / datatable
@@ -24,22 +25,7 @@ app_ui <- function(id = "app") {
   div(
     # http://jsfiddle.net/RichardHoultz/cxjje33y/
     class = "container-fluid",
-    tags$header(
-      class = "navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow",
-      tags$a(class = "navbar-brand col-md-3 col-lg-2 me-0 px-3", href = "#", "ndexr"),
-      tags$button(
-        class = "navbar-toggler position-absolute d-md-none collapsed",
-        type = "button", `data-bs-toggle` = "collapse",
-        `data-bs-target` = "#sidebarMenu", `aria-controls` = "sidebarMenu",
-        `aria-expanded` = "false", `aria-label` = "Toggle navigation",
-        tags$span(class = "navbar-toggler-icon")
-      ),
-      includeHTML("www/html/navbar.html"),
-      div(class = "navbar-nav", div(
-        class = "nav-item text-nowrap",
-        tags$a(class = "nav-link px-3", href = "#", "Sign Out")
-      ))
-    ),
+    header$header_ui(),
     includeHTML("www/html/dashboard.html"),
     includeScript("www/scripts/dashboard.js"),
     div(class = "container-fluid", div(
