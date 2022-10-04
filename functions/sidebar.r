3a8afdfb08fbd0affc2ab89862a40e9250131619
Freddy <drennanfreddy@gmail.com>
2022-10-04 02:43:35 +0000

    *  Author: freddy  *  Created on: Tue 04 Oct 2022 02:43:35 AM UTC

--- functions/sidebar.r
+++ functions/sidebar.r
@@ -23,6 +23,20 @@ sidebar_ui <- function(id = "sidebar") {
           )
         )
       )
+    ),
+    div(
+      class = "position-sticky pt-3",
+      tags$h6("Administration", class = "sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted"),
+      tags$ul(
+        class = "nav flex-column",
+        tags$li(
+          class = "nav-item",
+          tags$a(
+            class = "nav-link", href = "#",
+            tags$span(`data-feather` = "home", "Connections")
+          )
+        )
+      )
     )
   )
 }
