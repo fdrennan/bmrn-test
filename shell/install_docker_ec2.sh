d45eb03efaef73014d86fae09260e73235947775
Freddy <drennanfreddy@gmail.com>
2022-10-06 01:09:21 +0000

    *  Author: freddy  *  Created on: Thu 06 Oct 2022 01:09:21 AM UTC

--- shell/install_docker_ec2.sh
+++ shell/install_docker_ec2.sh
@@ -5,7 +5,7 @@ savelog "$LOGFILE"
 exec &> >(tee "$LOGFILE")
 
 apt-get update
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release -y
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release
 
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -13,10 +13,12 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
-usermod -aG docker $user
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+#mkdir -p $DOCKER_CONFIG/cli-plugins
+#curl -SL https://github.com/docker/compose/releases/download/v2.11.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+#chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 usermod -aG docker ubuntu
 
 # sudo usermod -aG docker $USER
