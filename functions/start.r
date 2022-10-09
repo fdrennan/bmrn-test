40e2c159f9bbbec20e19ff35da59da75043f8065
Freddy <drennanfreddy@gmail.com>
2022-10-09 01:02:33 +0000

    *  Author: freddy  *  Created on: Sun 09 Oct 2022 01:02:33 AM UTC

--- functions/start.r
+++ functions/start.r
@@ -18,6 +18,9 @@ ui <- function() {
   addResourcePath("loaders", "./www/images/loaders")
 
   tags$body(
+    HTML('<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">'),
     style = "max-height: 100vh; overflow-y: auto;",
     useShinyjs(),
     extendShinyjs(
