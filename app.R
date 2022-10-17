4b54c0b270c9948536a71ac9308e8ce241cc741b
Freddy <drennanfreddy@gmail.com>
2022-10-17 02:17:43 -0600

    *  Author: freddy  *  Created on: Mon 17 Oct 2022 02:17:43 AM MDT

--- app.R
+++ app.R
@@ -10,7 +10,7 @@ plan(multiprocess)
 options("test_version" = "Version 1.12")
 if (isTRUE(getOption("production"))) {
   options(require_validation = TRUE)
-  options(shiny.port = 5000, shiny.host = "0.0.0.0")
+  # options(shiny.host = "0.0.0.0")
   Sys.setenv(BASE_DOMAIN = "/qsci/test")
   options(send = TRUE)
   options("devmode" = FALSE)
